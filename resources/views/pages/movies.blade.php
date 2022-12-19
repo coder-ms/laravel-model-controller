adb3c9d1190f80c90e73b707b77f60bcaa087aa4
coder-ms <marco.sbrascia@gmail.com>
2022-12-19 22:46:46 +0200

    finito

--- resources/views/pages/movies.blade.php
+++ resources/views/pages/movies.blade.php
@@ -6,13 +6,26 @@
 
 @section('content')
     <h1>Movies</h1> 
-    <ul>
+    <div class="containerx">
         @forelse ($movies as $key=>$movie)
-            <li>{{$movie->title}}-{{$key}}</li>
+        <div class="flip-card">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">
+                <img src="{{$movie->image}}" alt="{{$movie->title}}">
+              </div>
+              <div class="flip-card-back">
+                <h3>{{$movie->title}}</h3>
+                <h5>{{$movie->original_title}}</h5>
+                <p>{{$movie->nationality}}</p>
+                <p>Data di uscita: {{$movie->date}}</p>
+                <p>Voto: {{$movie->vote}}/10</p>
+              </div>
+            </div>
+        </div>
         @empty
-            <li>NO movie</li>
+            <div>NO movie</div>
         @endforelse
-    </ul>
+    </div>
 
 @endsection
 
